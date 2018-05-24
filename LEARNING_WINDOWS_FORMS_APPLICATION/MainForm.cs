b89b3cca122f6528752a8132151c1d82e2560dad
Dariush Tasdighi <dariush.tasdighi@hotmail.com>
2018-05-24 10:56:04 +0430

    Add project files.

---
+++ LEARNING_WINDOWS_FORMS_APPLICATION/MainForm.cs
@@ -0,0 +1,68 @@
+﻿namespace LEARNING_WINDOWS_FORMS_APPLICATION
+{
+	public partial class MainForm : System.Windows.Forms.Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+
+		private void MainForm_Load(object sender, System.EventArgs e)
+		{
+		}
+
+		private void button1_Click(object sender, System.EventArgs e)
+		{
+			// Solution (1)
+			Form1 frm = new Form1();
+
+			frm.Show();
+
+			int intSomeNumber = 1;
+			// /Solution (1)
+
+			// Solution (2)
+			//Form1 frm = new Form1();
+
+			//frm.Show();
+
+			//textBox1.Text = "Hello, World!";
+			// /Solution (2)
+
+			// Solution (3)
+			//Form1 frm = new Form1();
+
+			//frm.MyMainForm = this;
+
+			//frm.Show();
+			// /Solution (3)
+		}
+
+		private void button2_Click(object sender, System.EventArgs e)
+		{
+			// Solution (1)
+			Form1 frm = new Form1();
+
+			frm.ShowDialog();
+
+			int intSomeNumber = 1;
+			// /Solution (1)
+
+			// Solution (2)
+			//Form1 frm = new Form1();
+
+			//frm.ShowDialog();
+
+			//textBox1.Text = "Hello, World!";
+			// /Solution (2)
+
+			// Solution (3)
+			//Form1 frm = new Form1();
+
+			//frm.MyMainForm = this;
+
+			//frm.ShowDialog();
+			// /Solution (3)
+		}
+	}
+}
